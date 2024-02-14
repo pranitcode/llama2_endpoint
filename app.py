919615c6a4f2e77b9bb5a44261524d29dc3053b3
pranitcode <pranitingole1@gmail.com>
2024-02-15 00:12:54 +0530

    .

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def summarize():
         prompt_text = data['text']
         prompt = "Summarize the following points in a point-by-point format: " + prompt_text 
         prompt += " Do not use any information not provided for creating summary points."
-        prompt += "summarize minimum 500 in point-by-point format: you can add small one line conclusion"
+        prompt += "summarize minimum 300 words in point-by-point format: you can add small one line conclusion"
        
         response_text = ""
         for event in replicate.stream(
@@ -27,7 +27,7 @@ def summarize():
                 "prompt": prompt,
                 "temperature": 0.75,
                 "system_prompt": "You are an incredibly helpful, respectful, and trustworthy assistant. Your expertise lies in your ability to skillfully summarize information with precision and clarity. Your proficiency as a summarizer is unmatched, making you an invaluable resource for anyone seeking concise and insightful summaries.",
-                "max_new_tokens": 300,
+                "max_new_tokens": 800,
                 "min_new_tokens": -1,
                 "repetition_penalty": 1
             },
