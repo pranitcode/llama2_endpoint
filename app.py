ef1afd84edb93ed44c4b1d13b474879ffeb445e4
pranitcode <pranitingole1@gmail.com>
2024-02-14 17:58:40 +0530

    prompt chage for asis

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def summarize():
                 "top_p": 1,
                 "prompt": prompt_text,
                 "temperature": 0.75,
-                "system_prompt": "You are a helpful,Summarize the provided text in 4 bullet points",
+                "system_prompt": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
                 "max_new_tokens": 800,
                 "min_new_tokens": -1,
                 "repetition_penalty": 1
